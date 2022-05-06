62e603d8c11b86d74c5839726d623cf850fe1be4
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-05-06 04:39:53 +0200

    display list of tasks in each category/lane

---
+++ src/pages/Board/Board.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import Lane from "../../components/Lane/Lane";
+import "./Board.css";
+
+const lanes = [
+  { id: 1, title: "To Do" },
+  { id: 2, title: "In Progress" },
+  { id: 3, title: "Review" },
+  { id: 4, title: "Done" },
+];
+
+function Board() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const tasks = await fetch(
+          `https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`
+        );
+        const result = await tasks.json();
+        if (result) {
+          setTasks(result);
+          setLoading(false);
+        }
+      } catch (err) {
+        setLoading(false);
+        setError(err.message);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="Board-wrapper">
+      {lanes.map((lane) => (
+        <Lane
+          key={lane.id}
+          title={lane.title}
+          loading={loading}
+          error={error}
+          tasks={tasks.filter((task) => task.lane === lane.id)}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Board;
