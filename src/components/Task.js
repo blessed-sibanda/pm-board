5e972736e371440f1fc8238d18b8c640fa09e409
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-05-06 05:57:01 +0200

    use styled components for styling

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,22 @@
-import "./Task.css";
+import styled from "styled-components";
+
+const TaskWrapper = styled.div`
+  background: darkgray;
+  padding: 20px;
+  border-radius: 20px;
+  margin: 0 5% 5% 5%;
+`;
+
+const Title = styled.h3`
+  width: 100%;
+  margin: 0;
+`;
 
 export default function Task({ id, title, body, onDragStart }) {
   return (
-    <div
-      draggable
-      onDragStart={(e) => onDragStart(e, id)}
-      className="Task-wrapper"
-    >
-      <h3>{title}</h3>
+    <TaskWrapper draggable onDragStart={(e) => onDragStart(e, id)}>
+      <Title>{title}</Title>
       <p>{body}</p>
-    </div>
+    </TaskWrapper>
   );
 }
